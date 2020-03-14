2ad39ca758c284ca7a27d9eca98430e5198738d1
hussain <nasiba.ansari@gmail.com>
2020-03-15 00:31:12 +0530

    typo

--- src/app/main/pages/CarList/CarList.js
+++ src/app/main/pages/CarList/CarList.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class CarList extends Component {
   render() {
     return (
       <div className="container flex justify-center items-center h-64 text-white h-full">
-        <h2>Car List Component</h2>
+        <h2>List Component</h2>
       </div>
     );
   }
--- src/app/main/pages/CarView/CarView.js
+++ src/app/main/pages/CarView/CarView.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class CarView extends Component {
   render() {
     return (
       <div className="container flex justify-center items-center h-64 text-white h-full">
-        <h2>Car View Component</h2>
+        <h2>View Component</h2>
       </div>
     );
   }
